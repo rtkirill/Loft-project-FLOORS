a1baaeea3e8120d6909350b583836b98a162a50c
Kirill <elpuffru@gmail.com>
2017-11-02 22:54:49 +0300

    add shop_card, fix bags: footer, resize in shop

--- js/main.js
+++ js/main.js
@@ -4,14 +4,18 @@ $(document).ready(function () {
 	$(".half").outerHeight($(".height-point").outerWidth()/2-5);
 	if($(window).width() > 1279) {
 		$(".news-article").outerHeight($(".height-point").outerWidth()*6+50);
+		$(".shop-card").outerHeight($(".height-point").outerWidth()*3+20);
 	}
+	
 	$(window).resize(function(){
 		$(".whole").outerHeight($(".height-point").outerWidth());
 		$(".half").outerHeight($(".height-point").outerWidth()/2-5);
 		if($(window).width() > 1279) {
 			$(".news-article").outerHeight($(".height-point").outerWidth()*6+50);
+			$(".shop-card").outerHeight($(".height-point").outerWidth()*3+20);
 		} else {
 			$(".news-article").css("height", "auto");
+			$(".shop-card").css("height", "auto");
 		}
 	});
 	//end "Высота блоков эквивалентна ширине"
