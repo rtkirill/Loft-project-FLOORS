34c95040208a2a5abbf2ea8190681a51327ff28d
Kirill <elpuffru@gmail.com>
2017-11-02 19:14:09 +0300

    add news.html

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
 	//Высота блоков эквивалентна ширине
 	$(".whole").outerHeight($(".height-point").outerWidth());
 	$(".half").outerHeight($(".height-point").outerWidth()/2-5);
+	$(".news-article").outerHeight($(".height-point").outerWidth()*6+50);
 	$(window).resize(function(){
 		$(".whole").outerHeight($(".height-point").outerWidth());
 		$(".half").outerHeight($(".height-point").outerWidth()/2-5);
+		$(".news-article").outerHeight($(".height-point").outerWidth()*6+50);
 	});
 	//end "Высота блоков эквивалентна ширине"
 });
