d30f1c8c05099e763186e33a1cf33a7b274f849c
Kirill <elpuffru@gmail.com>
2017-11-02 19:20:54 +0300

    [2] changes css and js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,17 @@ $(document).ready(function () {
 	//Высота блоков эквивалентна ширине
 	$(".whole").outerHeight($(".height-point").outerWidth());
 	$(".half").outerHeight($(".height-point").outerWidth()/2-5);
-	$(".news-article").outerHeight($(".height-point").outerWidth()*6+50);
+	if($(window).width() > 1279) {
+		$(".news-article").outerHeight($(".height-point").outerWidth()*6+50);
+	}
 	$(window).resize(function(){
 		$(".whole").outerHeight($(".height-point").outerWidth());
 		$(".half").outerHeight($(".height-point").outerWidth()/2-5);
-		$(".news-article").outerHeight($(".height-point").outerWidth()*6+50);
+		if($(window).width() > 1279) {
+			$(".news-article").outerHeight($(".height-point").outerWidth()*6+50);
+		} else {
+			$(".news-article").css("height", "auto");
+		}
 	});
 	//end "Высота блоков эквивалентна ширине"
 });
